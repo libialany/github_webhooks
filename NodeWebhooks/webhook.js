0b866df72c27ab7517c71e48b054c62cd964c9e9
libialany <elibialany@gmail.com>
2022-09-20 22:35:28 +0000

    add scripts

---
+++ NodeWebhooks/webhook.js
@@ -0,0 +1,16 @@
+var secret = "d0n7s4y!Opsl0v3iitt"
+var repo = "/home/libia/github_action2/"
+
+const http=require('http');
+const crypto=require('crypto');
+const exec = require('child_process').exec;
+http.createServer(function (req, res) {
+    req.on('data', function(chunk) {
+        let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
+
+        if (req.headers['x-hub-signature'] == sig) {
+            exec('cd ' + repo + ' && git pull');
+        }
+    });
+    res.end();
+}).listen(8000);
